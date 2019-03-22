e9499be036e3880c6d7af073371307bb9fdee55e
William Bergh <william.bergh1@learnet.se>
2019-03-22 12:52:34 +0100

    bug fix

--- assets/inc/login.inc.php
+++ assets/inc/login.inc.php
@@ -1,4 +1,4 @@
-<form class="form__login" action="../script/login.script.php" method="post">
+<form class="form__login" action="/dashboard/assets/script/login.script.php" method="post">
   <input type="text" name="login_username" placeholder="Username">
   <input type="password" name="login_password" placeholder="Password">
   <button type="submit" name="login_button">Login</button>
