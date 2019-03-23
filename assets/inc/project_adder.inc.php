4b3e2e7e0487b9d958fbb1c280f9bc6df1298d0e
William Bergh <william.bergh1@learnet.se>
2019-03-23 12:22:15 +0100

    added a form to ad more projects to main website

---
+++ assets/inc/project_adder.inc.php
@@ -0,0 +1,17 @@
+<div class="project_adder_form_container">
+  <form class="project_adder_form" action="index.html" method="post">
+    <input class="project_adder_form_input" type="text" name="project_titel" placeholder="Title">
+    <input class="project_adder_form_input" type="text" name="project_features" placeholder="Features">
+    <input class="project_adder_form_input" type="text" name="project_description" placeholder="Description">
+    <div class="project_lang_container">
+      <div><input class="project_adder_form_checkbox" type="checkbox" name="project_lang">HTML</div>
+      <div><input class="project_adder_form_checkbox" type="checkbox" name="project_lang">CSS</div>
+      <div><input class="project_adder_form_checkbox" type="checkbox" name="project_lang">PHP</div>
+      <div><input class="project_adder_form_checkbox" type="checkbox" name="project_lang">MySQL</div>
+      <div><input class="project_adder_form_checkbox" type="checkbox" name="project_lang">JavaScript</div>
+    </div>
+    <input class="project_adder_form_input" type="text" name="project_link" placeholder="Github Link">
+    <button class="project_adder_form_button" type="button" name="project_add_button">Add Project</button>
+  </form>
+
+</div>
