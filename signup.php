2b812d73ba9222333ffc55e27b959820a97d6c39
William Bergh <william.bergh1@learnet.se>
2019-03-23 12:25:56 +0100

    minor changes

--- signup.php
+++ signup.php
@@ -12,20 +12,12 @@
 <div class="formContainer">
   <h1>Sign up</h1>
   <form class="form" action="./assets/script/signup.script.php" method="post">
-    <div class="form_username">
-      <label for="username">Username</label>
       <?php if ($username_error == "taken") {echo "<p>username already registered</p>";} ?>
       <input type="text" name="username" required="required" placeholder="username">
-    </div>
-    <div class="form_password">
-      <label for="passowrd">Password</label>
       <?php if ($password_error == "no_match") {echo "<p>passwords didn't match</p>";}
         else if ($password_error == "hash_error") {echo "<p>something went wrong, please try again</p>";} ?>
       <input type="password" name="password" required="required" placeholder="password">
       <input type="password" name="password_repeat" required="required" placeholder="repeat password">
-    </div>
-    <div class="form_button">
       <button type="submit" name="signup_button">Signup</button>
-    </div>
   </form>
 </div>
