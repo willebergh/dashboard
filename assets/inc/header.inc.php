9e74c023758d1feb3c763f629ab7a80c1524eb49
William Bergh <william.bergh1@learnet.se>
2019-03-23 12:18:35 +0100

    added header.inc.php

---
+++ assets/inc/header.inc.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="/dashboard/assets/css/reset.css">
+    <link rel="stylesheet" href="/dashboard/assets/css/main.css">
+  </head>
+  <body>
+    <header>
+      
+    </header>
