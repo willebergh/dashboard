d66ac6ffe1a8d0d6b5079ecb4df9d133ca05ece4
William Bergh <william.bergh1@learnet.se>
2019-03-23 12:21:06 +0100

    added more html to login page

--- assets/inc/login.inc.php
+++ assets/inc/login.inc.php
@@ -1,5 +1,20 @@
-<form class="form__login" action="/dashboard/assets/script/login.script.php" method="post">
-  <input type="text" name="login_username" placeholder="Username">
-  <input type="password" name="login_password" placeholder="Password">
-  <button type="submit" name="login_button">Login</button>
-</form>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Dashboard Login</title>
+    <link rel="stylesheet" href="/dashboard/assets/css/reset.css">
+    <link rel="stylesheet" href="/dashboard/assets/css/login.css">
+  </head>
+  <body>
+    <div class="form_container">
+      <p>Welcome to the Dashboard</p>
+      <p>Please enter username and password</p>
+      <form class="login_form" action="/dashboard/assets/script/login.script.php" method="post">
+        <input class="login_input" type="text" name="login_username" placeholder="Username">
+        <input class="login_input" type="password" name="login_password" placeholder="Password">
+        <button class="login_button" type="submit" name="login_button">Login</button>
+      </form>
+    </div>
+  </body>
+</html>
